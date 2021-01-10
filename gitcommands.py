933390a9ff6507df9d19224846be69c3791f612c
Antriksh Misri <54466356+antrikshmisri@users.noreply.github.com>
2021-01-10 15:38:15 +0530

    fix: fixed git commands call

--- gitcommands.py
+++ gitcommands.py
@@ -1,15 +1,15 @@
 from subprocess import call
 from sys import platform as _platform
 def init():
-    print('git init')
+    call('git init')
 # git add <filename>
 def createReadme():
     if _platform == "linux" or _platform == "linux2":
-        print('touch README.md')
+        call('touch README.md')
     elif _platform == "darwin":
-        print('touch README.md')
+        call('touch README.md')
     elif _platform == "win32":
-        print('type nul>README.md')
+        call('type nul>README.md')
 
 
 # Windows
@@ -17,7 +17,7 @@ def add(filelist):
     for file in filelist:
         # perform git add on file
         print("Adding" , file)
-        print(('git add ' + file))
+        call(('git add ' + file))
 
 # git commit -m "passed message"
 def commit(filelist):
@@ -34,9 +34,9 @@ def commit(filelist):
         #added a comment
         else:
             filelist.remove(file)
-            print('git commit -m "' + msg + '"')
+            call('git commit -m "' + msg + '"')
             call('cls' , shell=True)
 # git push
 def push(url , branch):
-    print('git push -u ' + url + ' ' + branch)
+    call('git push -u ' + url + ' ' + branch)
     #added a comment
