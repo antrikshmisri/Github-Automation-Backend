c15c4d2e7205e30ed0b9522836397859f4a2384b
antrikshmisri <antrikshmisri@gmail.com>
2021-02-08 15:33:48 +0530

    feat: added colors to logs

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-import repoInfo,filechange
+import repoInfo
+import filechange
 import gitcommands as git
+from colors import logcolors
 import pyfiglet
+
+
 def init():
     info = repoInfo.checkinfoInDir()
+    url, branch = info
     if('n' in info):
         info.remove('n')
         git.init()
         git.createReadme()
         git.add(['.'])
         git.commit(['README.md'])
-        git.setBranch(info[1])
-        git.setremote(info[0])
-        git.push(info[0] , info[1])
+        git.setBranch(branch)
+        git.setremote(url)
+        git.push(url, branch)
         print('initial setup done :)')
-        filechange.ischanged(info[0] , info[1])
+        filechange.ischanged(url, branch)
     else:
-        print('Retrieving info from git directory')
-        filechange.ischanged(info[0] , info[1])
+        print(f'{logcolors.BOLD}Retrieving info from git directory{logcolors.ENDC}')
+        print(f'{logcolors.CYAN}URL:{logcolors.ENDC} {url} , {logcolors.CYAN}Branch:{logcolors.ENDC} {branch}')
+        filechange.ischanged(info[0], info[1])
+
 
 if __name__ == '__main__':
-    f = pyfiglet.figlet_format('G - AUTO' , font = '5lineoblique')
-    print(f)
+    f = pyfiglet.figlet_format('G - AUTO', font='5lineoblique')
+    print(f"{logcolors.BOLD}{f}{logcolors.ENDC}")
     init()
--- repoInfo.py
+++ repoInfo.py
@@ -1,23 +1,27 @@
+import subprocess
 import os
 mypath = os.getcwd()
 infofile = mypath + '/.git/config'
-import subprocess
+
+
 def takeInfo():
     print('No Existing repo info found\n')
     url = str(input('Enter the Github Repo URL: '))
     branch = str(input('Enter the branch: '))
-    info = ['n' , url , branch]
+    info = ['n', url, branch]
     return info
+
 
 def checkinfoInDir():
     if (os.path.exists(infofile)):
-        print('Repo Info Found:-')
-        url = subprocess.Popen('git config --get remote.origin.url' , stdout=subprocess.PIPE).stdout.read().decode('utf-8')
-        branch = subprocess.Popen('git rev-parse --symbolic-full-name HEAD' , stdout=subprocess.PIPE).stdout.read().decode('utf-8')
-        url,branch = url.split('\n')[0],branch.split('\n')[0].split('/')[2]
-        info = [url , branch]
+        url = subprocess.Popen('git config --get remote.origin.url',
+                               stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+
+        branch = subprocess.Popen('git rev-parse --symbolic-full-name HEAD',
+                                  stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+                                  
+        url, branch = url.split('\n')[0], branch.split('\n')[0].split('/')[2]
+        info = [url, branch]
     else:
         info = takeInfo()
     return info
-
-print(checkinfoInDir())
