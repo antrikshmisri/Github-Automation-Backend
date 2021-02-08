02c3374b86832afcada3a05e3c37d6ce9b054aa0
antrikshmisri <antrikshmisri@gmail.com>
2021-02-08 15:24:33 +0530

    feat: added colors to logs

--- diffcalc.py
+++ diffcalc.py
@@ -1,5 +1,6 @@
-
+from colors import logcolors
 import difflib
+
 
 def getMaxSpaces(file):
     max = float('-inf')
@@ -9,13 +10,16 @@ def getMaxSpaces(file):
             max = len(ele)
     return max
 
-def calcDiff(firstFile , secondFile):
-    diff = difflib.ndiff(firstFile , secondFile)
+
+def calcDiff(firstFile, secondFile):
+    diff = difflib.ndiff(firstFile, secondFile)
     deltainit = ''.join(x[2:] for x in diff if x.startswith('+ '))
     deltainit = deltainit.split('\n')
     maxspacesinit = getMaxSpaces(deltainit)
-    print('CHANGED LINES ARE:-\n' , '-' * maxspacesinit)
+    print(f'{logcolors.BOLD}CHANGED LINES ARE{logcolors.ENDC}\n',
+          f'{logcolors.BOLD}-{logcolors.ENDC}' * maxspacesinit)
     for ele in deltainit:
-        print(str(ele.strip()) , ' ' * (maxspacesinit - len(ele.strip())), '+')
-    print('' , '-' * maxspacesinit)
-    
+        print(f'{logcolors.SUCCESS}{str(ele.strip())}{logcolors.ENDC}',
+              ' ' * (maxspacesinit - len(ele.strip())), '+')
+    print('', f'{logcolors.BOLD}-{logcolors.ENDC}' * maxspacesinit)
+    return deltainit
