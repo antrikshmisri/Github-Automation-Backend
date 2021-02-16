ac39ac6fe99419f440fc64c57e0d584985a19c01
antrikshmisri <antrikshmisri@gmail.com>
2021-02-16 14:05:20 +0530

    added comments

--- diffcalc.py
+++ diffcalc.py
@@ -12,14 +12,19 @@ def getMaxSpaces(file):
 
 
 def calcDiff(firstFile, secondFile):
+    # calculate raw diff
     diff = difflib.ndiff(firstFile, secondFile)
+    # calculate unique lines in secondfile
     deltainit = ''.join(x[2:] for x in diff if x.startswith('+ '))
+    # reformat the lines
     deltainit = deltainit.split('\n')
     maxspacesinit = getMaxSpaces(deltainit)
     print(f'{logcolors.BOLD}CHANGED LINES ARE{logcolors.ENDC}\n',
           f'{logcolors.BOLD}-{logcolors.ENDC}' * maxspacesinit)
+
     for ele in deltainit:
-        print(f'{logcolors.SUCCESS}{str(ele.strip())}{logcolors.ENDC}',
+        print(f'{logcolors.SUCCESS} {str(ele.strip())} {logcolors.ENDC}',
               ' ' * (maxspacesinit - len(ele.strip())), '+')
+              
     print('', f'{logcolors.BOLD}-{logcolors.ENDC}' * maxspacesinit)
     return deltainit
