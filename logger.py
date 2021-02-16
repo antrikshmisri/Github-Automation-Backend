df405e7fcec923acef2b1ec653121ba12dd2a5d9
antrikshmisri <antrikshmisri@gmail.com>
2021-02-16 14:12:18 +0530

    fix: fixed read and update operations

--- logger.py
+++ logger.py
@@ -1,6 +1,7 @@
 import json
 import os
-
+from colors import logcolors
+import filechange
 jsonpath = os.path.join(os.getcwd(), 'auto-scripts\\tmp.json')
 buffer = []
 
@@ -23,21 +24,37 @@ def writedata(*args, **kwargs):
             json.dump([obj for obj in buffer], file, indent=4)
 
 
-def readdata(filename, diff):
+def updatedata(filename, diffarr):
     if(os.path.getsize(jsonpath) > 0):
         with open(jsonpath, 'r') as file:
             readdata = json.load(file)
         if(len(readdata) == 0):
             print('No changed file left')
         else:
+            tmpdata,tmpfile,tmpdiff = readdata.copy(),filename.copy(),diffarr.copy()
             print('Found some changed files')
-            for file in filename:
+            for file,diff in zip(filename,diffarr):
                 print('Removing ' + str(file) + ' from json file')
                 for obj in readdata:
                     if obj['path'] == file and obj['changes'] == diff:
-                        readdata.remove(obj)
-                filename.remove(file)
-            writedata(buffer=readdata)
+                        tmpdata.remove(obj)
+                        tmpfile.remove(file)
+                        tmpdiff.remove(diff)
+            # make the original lists empty without changing address
+            del filename[:],diffarr[:]
+            writedata(buffer=tmpdata)
 
     else:
         print('No data to read')
+
+
+def checkdata(url , branch):
+    if(os.path.getsize(jsonpath) > 0):
+        with open(jsonpath, 'r') as file:
+            initdata = json.load(file)
+        if(len(initdata) == 0):
+            print(f'{logcolors.SUCCESS}Change tree clean{logcolors.ENDC}')
+        else:
+            filechange.ischanged(url , branch , initbuffer = initdata)
+    else:
+        print(f'{logcolors.ERROR}No changes found from previous session{logcolors.ENDC}')
